d697db6ee28a7502cf106390e6c8e39777e064df
Caukx07 <145811369+Cau407@users.noreply.github.com>
2024-05-06 18:52:46 -0300

    version 0.0.1

---
+++ frontend-editor-2024-05-06T21_52_05.289Z/index.js
@@ -0,0 +1,26 @@
+// var name = String(promt('Qual seu nome?'))
+// var age = Number(promt('Qual sua idade?'))
+
+// let rodolfo = {
+//   name: name,
+//   age: age,
+//   isHuman: true
+// }
+
+// console.log(`Olá, ${rodolfo.name}, você tem ${rodolfo.age} anos`)
+
+let bernardo = [
+  'girafa',
+  'zebra',
+  25,
+  'alfabeto',
+  {
+    name: 'luan',
+    age: 26,
+    isAdm: true
+  },
+  'sla',
+  true
+]
+
+console.log(`A quinta linha do seu Array é ${bernardo[4].name}`)
